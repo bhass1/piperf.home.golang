bce33d6100579af93dd6b7da094db76c167dbc33
Bill Hass <billhass@umich.edu>
2019-09-07 02:54:52 +0000

    Initial commit Working client based on mosquitto example. Iperf tries a few servers before failing. Sends results in JSON form to aws_mqtt_broker.

---
+++ do_iperf.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+iperf3 -c iperf.he.net -n 3M -N -R -J -p 5201
+if [ $? -eq 1 ]; then
+	iperf3 -c iperf.scottlinux.com -n 3M -N -R -J -p 5201
+fi
+if [ $? -eq 1 ]; then
+	iperf3 -c ping.online.net -n 3M -N -R -J -p 5207
+fi
+if [ $? -eq 1 ]; then
+	iperf3 -c bouygues.iperf.fr -n 3M -N -R -J -p 5207
+fi
